9b4ef4276d6731c00d34250e5ade750d4c9e7e41
cbcwestwolf <1004626265@qq.com>
2023-05-15 16:35:45 +0800

    before add user 'cbc'

--- entrypoint.total.sh
+++ entrypoint.total.sh
@@ -4,6 +4,13 @@ ulimit -n 1024
 service slapd start
 # service krb5-kdc start
 
+# # Updating the master key: https://web.mit.edu/kerberos/krb5-latest/doc/admin/database.html#updating-master-key
+# printf "123456\n123456" | kdb5_util add_mkey -s
+# kdb5_util use_mkey 2
+# printf "123456\n123456" | kinit yangkeao
+
 ./tidb-server &
+sleep 3
+mysql -h 127.0.0.1 -u root -P 4000 -e "set global authentication_ldap_sasl_server_host='127.0.0.1';create user yangkeao IDENTIFIED WITH authentication_ldap_sasl as 'cn=yangkeao+uid=yangkeao,dc=example,dc=org';"
 
 exec "$@"
