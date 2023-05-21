0203eb23f7c2272c62a149028669d96dde3c743a
cbcwestwolf <1004626265@qq.com>
2023-05-21 16:29:22 +0800

    Succeed to authenticate with GSSAPI :tada:

--- entrypoint.total.sh
+++ entrypoint.total.sh
@@ -7,10 +7,10 @@ service krb5-kdc start
 
 printf "123456" | kinit cbc
 
-# ./tidb-server &
-# sleep 3
-# mysql -h 127.0.0.1 -u root -P 4000 -e "set global authentication_ldap_sasl_server_host='127.0.0.1';"
-# mysql -h 127.0.0.1 -u root -P 4000 -e "create user yangkeao IDENTIFIED WITH authentication_ldap_sasl as 'cn=yangkeao+uid=yangkeao,dc=example,dc=org';"
-# mysql -h 127.0.0.1 -u root -P 4000 -e "create user cbc IDENTIFIED WITH authentication_ldap_sasl as 'uid=cbc,dc=example,dc=org';"
+./tidb-server &
+sleep 3
+mysql -h 127.0.0.1 -u root -P 4000 -e "set global authentication_ldap_sasl_server_host='127.0.0.1';"
+mysql -h 127.0.0.1 -u root -P 4000 -e "create user yangkeao IDENTIFIED WITH authentication_ldap_sasl as 'cn=yangkeao+uid=yangkeao,dc=example,dc=org';"
+mysql -h 127.0.0.1 -u root -P 4000 -e "create user cbc IDENTIFIED WITH authentication_ldap_sasl as 'uid=cbc,dc=example,dc=org';"
 
 exec "$@"
