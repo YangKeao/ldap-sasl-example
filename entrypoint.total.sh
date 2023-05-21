be037f7374f9ac2d9305f551b2cd9aa390a4ef9e
cbcwestwolf <1004626265@qq.com>
2023-05-21 16:07:57 +0800

    Finish AWS Guide (2/2) https://aws.amazon.com/cn/blogs/china/amazon-emr-authentication-scheme-based-on-openldap-and-kerberos-iii-deep-integration-based-on-sasl-gssapi/
    TODO: integrate with TiDB

--- entrypoint.total.sh
+++ entrypoint.total.sh
@@ -7,14 +7,10 @@ service krb5-kdc start
 
 printf "123456" | kinit cbc
 
-# # Updating the master key: https://web.mit.edu/kerberos/krb5-latest/doc/admin/database.html#updating-master-key
-# printf "123456\n123456" | kdb5_util add_mkey -s
-# kdb5_util use_mkey 2
-
 # ./tidb-server &
 # sleep 3
 # mysql -h 127.0.0.1 -u root -P 4000 -e "set global authentication_ldap_sasl_server_host='127.0.0.1';"
 # mysql -h 127.0.0.1 -u root -P 4000 -e "create user yangkeao IDENTIFIED WITH authentication_ldap_sasl as 'cn=yangkeao+uid=yangkeao,dc=example,dc=org';"
-# mysql -h 127.0.0.1 -u root -P 4000 -e "create user cbc IDENTIFIED WITH authentication_ldap_sasl as 'cn=cbc+uid=cbc,dc=example,dc=org';"
+# mysql -h 127.0.0.1 -u root -P 4000 -e "create user cbc IDENTIFIED WITH authentication_ldap_sasl as 'uid=cbc,dc=example,dc=org';"
 
 exec "$@"
